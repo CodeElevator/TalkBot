824452241f3ce26f013f1a1a3ca85632b933187e
CodeElevator <98259474+CodeElevator@users.noreply.github.com>
2022-02-11 11:01:46 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+# Pycord packages
+import discord
+from discord.ext import commands
+
+
+class Bot(commands.Bot):
+  def __init__(self, *args, **kwargs):
+    super.__init__(*args, **kwargs)
+  async def on_ready(self):
+    print("bot is online!")
+
+#OPTIONAL COMMAND PREFIX
+client = Bot(command_prefix="!")
+
+@client.event
+async def on_message(message):
+  if message.content.startwith("Quel est ton prefixe?"):
+    await message.channel.send("Mon prefixe est: ! mais vous n'avez pas besoin de l'utiliser.")
+  if message.content.startwith("Quel temps fera-t-il demain?):
+    await message.channel.send("je n'en ai aucune idee")
+
+ 
+client.run(token)
